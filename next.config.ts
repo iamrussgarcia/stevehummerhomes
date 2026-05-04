2aa12d1d3931cd5824532582518e8cf0664da3b9
Russ Garcia-PC <russpeteregarcia@gmail.com>
2026-05-05 06:56:56 +0800

    feat: enable React compiler, set standalone output, and add root redirect to home page

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
